3595bc74f97754b6b071552d66b3d5957a511d92
ranicholson <ranicho2@gmail.com>
2021-05-14 21:20:12 +0000

    Task 2 updates

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,18 +1,11 @@
-// jQuery script to listen to checkboxes
-$(document).ready(function () {
-  $('input[type="checkbox"]').click(function (data) {
-    const amenityIds = {};
-    if ($(this).prop('checked') === true) {
-      amenityIds[data.id] = data.name;
-    } else if ($(this).prop('checked') === false) {
-      delete amenityIds[data.id];
+$('document').ready(function () {
+  const amenityIds = {};
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenityIds[$(this).attr('data-id')];
     }
-    let amenityNames = [];
-    for (const key in amenityIds) {
-      amenityNames.push(amenityIds[key]);
-    }
-    amenityNames[0] = "test";
-    console.log(amenityIds);
-    $('.amenities h4').text(amenityNames);
+    $('.amenities h4').text(Object.values(amenityIds).join(', '));
   });
 });
